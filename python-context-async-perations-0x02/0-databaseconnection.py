64317a42ce4eaf41367f6cec7cf3c7ccb78b71c2
Yonatan <107502940+Biyuktul@users.noreply.github.com>
2025-05-21 15:50:35 +0300

    refactoring

--- python-context-async-perations-0x02/0-databaseconnection.py
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -4,8 +4,8 @@ class DatabaseConnection:
     def __init__(self, db_name):
         self.db_name = db_name
     def __enter__(self):
-        conn = sqlite3.connect(self.db_name)
-        return conn
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.conn.close()
 
