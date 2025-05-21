e5227a8d998813fa447ee9decd7f6ef1a0c7e174
Yonatan <107502940+Biyuktul@users.noreply.github.com>
2025-05-21 15:44:35 +0300

    context manager to manage db connectin with custom context class

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,10 @@
+import sqlite3
+ 
+class DatabaseConnection:
+    def __init__(self, db_name):
+        self.db_name = db_name
+    def __enter__(self):
+        conn = sqlite3.connect(self.db_name)
+        return conn
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.close()
