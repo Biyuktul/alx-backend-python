07956a55040a0bf2d17f71bb1a95438a49949935
Yonatan <107502940+Biyuktul@users.noreply.github.com>
2025-06-06 13:20:16 +0300

    adding filters

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -6,7 +6,7 @@ class IsParticipantOfConversation(permissions.BasePermission):
     Custom permission to:
     - Allow only authenticated users.
     - Allow only participants of a conversation to access it.
-    - Allow only message senders to edit or delete their oown messages.
+    - Allow only message senders to edit or delete their messages.
     """
 
     def has_permission(self, request, view):
@@ -14,11 +14,15 @@ class IsParticipantOfConversation(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         if hasattr(obj, 'participants'):
+            # obj is a Conversation
             return request.user in obj.participants.all()
 
         elif hasattr(obj, 'conversation'):
+            # obj is a Message
+            # Check participant access for viewing
             if request.method in SAFE_METHODS:
                 return request.user in obj.conversation.participants.all()
+            # For edits or deletes, only the sender can act
             elif request.method in ['PUT', 'PATCH', 'DELETE']:
                 return request.user == obj.sender
 
